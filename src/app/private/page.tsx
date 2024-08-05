0e55118b46d425ce39e92b669f4b4ad8e0461dd4
Jean Robertou <j-rob@live.fr>
2024-08-05 08:27:28 +0200

    feat: add supabse connection

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,3 +1,13 @@
-export default function Private() {
-  return <div>Private page</div>;
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function PrivatePage() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
+  return <p>Hello {data.user.email}</p>;
 }
