554fab4dc0636db7b552164b9a5843bf6afdd18a
Jean Robertou <j-rob@live.fr>
2024-08-08 08:52:55 +0200

    feat: add types and supabase doc

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,10 +1,11 @@
+import { Database } from "./database.types";
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function createClient() {
   const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
